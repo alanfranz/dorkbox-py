7a4302b7357c0484753f935171ce95b4d0ceefb4
Alan Franzoni <username@franzoni.eu>
2016-08-25 16:52:43 +0200

    Enh: new test conversion

--- dorkbox/test/test_dorkbox.py
+++ dorkbox/test/test_dorkbox.py
@@ -2,7 +2,7 @@
 from unittest import TestCase
 from tempfile import TemporaryDirectory
 import os
-from subprocess import check_call
+from subprocess import check_call, check_output
 
 from dorkbox.dorkbox import Git, Repository, GITIGNORE
 
@@ -22,3 +22,19 @@ class TestRepository(TestCase):
             check_call(["git", "init", "--bare", gitrepodir])
             Repository.create_new(os.path.abspath("."), gitrepodir)
             self.assertTrue(os.path.exists("./{}".format(GITIGNORE)))
+
+    def test_when_connection_new_dorkbox_repo_both_references_exist(self):
+        with TemporaryDirectory() as git_repo_dir:
+            check_call(["git", "init", "--bare", git_repo_dir])
+
+            first_client_name = None
+
+            with TemporaryDirectory() as first_repo_dir:
+                first_repo = Repository.create_new(first_repo_dir, git_repo_dir)
+                with TemporaryDirectory() as second_repo_dir:
+                    second_repo = Repository.connect_existing(second_repo_dir, git_repo_dir)
+                    all_branches = check_output(["git", "--work-tree={}".format(second_repo_dir),
+                                                 "--git-dir={}".format(os.path.join(second_repo_dir, ".git")),
+                                                 "branch", "-a"], universal_newlines=True)
+                    self.assertTrue(first_repo.client_id in all_branches)
+                    self.assertTrue(second_repo.client_id in all_branches)
